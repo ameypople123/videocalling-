f0ef0798aa22e2f57ef31aa49a713be330f6d6fa
ameypople123 <80151083+ameypople123@users.noreply.github.com>
2021-04-27 10:05:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var app = require('express')();
+var cors = require('cors');
+var server = require('http').Server(app);
+var io = require('socket.io')(server, { origins: '*:*' });
+const PORT = process.env.PORT || 3000
+app.use(cors())
+
+var room = {};
+
+server.listen(PORT);
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log(socket.id + ' had connected')
+    console.log(room)
+
+    socket.on('message', data => {
+        data = JSON.parse(data)
+        if (!room[data.room]) {
+            room[data.room] = [socket.id]
+        } else if (room[data.room].length < 2) {
+            if (room[data.room].filter(i => i === socket.id).length === 0) {
+                room[data.room].push(socket.id)
+            }
+        }
+        if (room[data.room].length === 2) {
+            for (s of room[data.room]) {
+                if (s !== socket.id && io.sockets.sockets[s] && room[data.room].length <= 2) {
+                    io.sockets.sockets[s].emit('message', data.data)
+                }
+            }
+        }
+    })
+
+    socket.on('join', (roomId) => {
+        console.log('join' + roomId)
+        if (room[roomId] && room[roomId].length === 2) {
+            socket.emit('reject', { error: 'Room is full' })
+        }
+    })
+
+    socket.on('leave', (roomId) => {
+        console.log('leave' + roomId)
+        for (s in room) {
+            for (let i = 0; i < room[s].length; i++) {
+                if (room[s][i] === roomId) {
+                    room[s].splice(i, 1)
+                    if (room[s].length == 0) {
+                        delete room[s]
+                    }
+                    break
+                }
+            }
+        }
+        console.log(room)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(socket.id + ' has been disconnected')
+        for (s in room) {
+            for (let i = 0; i < room[s].length; i++) {
+                if (room[s][i] === socket.id) {
+                    room[s].splice(i, 1)
+                    if (room[s].length == 0) {
+                        delete room[s]
+                    }
+                    break
+                }
+            }
+        }
+        console.log(room)
+    })
+});
